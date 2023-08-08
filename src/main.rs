2ce7cf29e825a11971009463b9471852eee524ac
andilar <andilar@users.noreply.github.com>
2023-08-08 08:45:24 +0000

    initial test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, World!");
+    println!("Da bin ich..");
+    println!("Hello, Nicole!");
+    println!("...bing..");
+
 }
+
+fn check () {}
